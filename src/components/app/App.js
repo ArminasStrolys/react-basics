30ccce9d0dcd8b508627436baad94ac3ab03d757
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-03-09 18:46:12 +0200

    Basics training

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cube">
+        <p>Hello world!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
