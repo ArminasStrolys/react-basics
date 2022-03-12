7d610a32d8472eb1e4a19408c7389668efe22842
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-03-12 10:20:41 +0200

    form

--- src/components/greetForm/GreetForm.jsx
+++ src/components/greetForm/GreetForm.jsx
@@ -1,18 +1,22 @@
 import React, {useState} from 'react'
+import './greetForm.css'
 
 export default function GreetForm() {
     function handleClick(e) {
         e.preventDefault();
         console.log(name);
-        alert(`Hey, ${name} surname`);
+        alert(`Hey, ${name} ${surName}`);
+        setName('');
+        setSurName('');
       }
       const [name, setName] = useState('')
+      const [surName, setSurName] = useState('')
   return (
-    <form>
-        <input onChange={setName} type="text" placeholder='Name'/>
+    <form className='form-6'>
+        <input value={name} onChange={e=>setName(e.target.value)} type="text" placeholder='Name'/>
         {console.log(setName)}
         <br />
-        <input type="text" placeholder='Surname'/>
+        <input value={surName} onChange={e=>setSurName(e.target.value)} type="text" placeholder='Surname'/>
         <br />
         <button onClick={handleClick} type="submit">ENTER</button>
     </form>
